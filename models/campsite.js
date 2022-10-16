b2296fdeada6b2cd4f8f494a50a47969d3dd971f
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-16 09:59:09 +0000

    express rest api with mongodb and mongoose 1

---
+++ models/campsite.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+// create a schema for comments about campsites
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// create a schema for campsite
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema] //add comments array as sub document in campsites schema
+}, {
+    timestamps: true
+});
+
+// create model using the campsite schema
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
