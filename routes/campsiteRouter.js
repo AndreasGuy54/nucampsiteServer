569e66efc9c3945745bef5de6516db34ed884576
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-21 15:29:44 +0000

    mongoose population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,11 +8,12 @@ const campsiteRouter = express.Router();
 // setting up routes for /campsites
 campsiteRouter.route('/')
 .get((req, res, next) => {
-    Campsite.find() //query db for all documents instantiated with campsite model
+    Campsite.find()
+    .populate('comments.author')
     .then(campsites => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(campsites); //send json data to client and close response stream
+        res.json(campsites);
     })
     .catch(err => next(err));
 })
@@ -44,6 +45,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -80,6 +82,7 @@ campsiteRouter.route('/:campsiteId')
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -97,6 +100,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -144,6 +148,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
